c4b096f3f39055ea811f9e9714b03baeede701b7
Roma <roma@roma-HP-620.(none)>
2013-10-20 11:20:18 +0300

    created abstract class

---
+++ classes/BaseFunctions_class.php
@@ -0,0 +1,23 @@
+<?php
+
+abstract class BaseFunctions {
+
+    var $is_load;
+
+    abstract function reload();
+
+    function makeShot()
+    {
+        if ($this->is_load == 1){
+            echo "piu-piu";
+        } else {
+            echo "not loaded";
+            $this->reload();
+        }
+    }
+
+    function makeAim()
+    {
+        echo "target acquired";
+    }
+}
