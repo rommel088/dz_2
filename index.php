29f2e4a9fcbf7777b503d20e212142d561d30910
rommel <rommel0888@gmail.com>
2013-10-21 12:02:30 +0300

    added autoload and su100 object

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
 <?php
-require_once("classes/Howitzer_class.php");
+//require_once("classes/Howitzer_class.php");
+
+function __autoload($name)
+{
+    require_once ("classes/".$name."_class.php");
+}
 
 $m30 = new Howitzer();
 $m30->setReloadTime(10);
 $m30->reload();
 $m30->makeAim();
 $m30->makeShot();
+
+$SU100 = new SpCannon();
+$SU100->setReloadTime(8);
+$SU100->setEngine(520);
+$SU100->moveTo("hill");
+$SU100->reload();
+$SU100->makeAim();
+$SU100->makeShot();
